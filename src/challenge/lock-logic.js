33d97882bf8fde2806184e4e55579844ea5e6910
Thom Mondeaux <Thommond554@gmail.com>
2021-02-04 14:35:50 -0500

    Updated the lock-logic.js file which now the user can put in the combo to then have the program validate it with the SECRET_COMBO var then the user is redirected to my profile.

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -7,9 +7,24 @@ const lockState = window.mobx.observable({
   wheels: [0, 0, 0, 0]
 })
 
+// Checks if contents of array are equal
+function isEqual (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
+  };
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (!arr2.includes(arr1[i])) {
+      return false
+    };
+  }
+  return true
+}
+
 function changeDialValue (index, incrementBy) {
   // This part is missing some code
   // This function is automatically called when the user clicks on a chevron
+
   // it will be called with a wheel index and an amount to change the value by
   // for example, if a user clicks on the "up" arrow for wheel 0
   // this will be called with arguments (0, 1) indicating we should raise the first dial's value by one
@@ -20,6 +35,17 @@ function changeDialValue (index, incrementBy) {
   // lockState.locked = false
   // or lockState.wheels[1] = 2
   // the lock will re-render itself when the value changes
+
+  if (isEqual(SECRET_COMBO, lockState.wheels)) {
+    lockState.locked = false
+    redirect('Thomas-Mondeaux')
+  }
+
+  if (incrementBy === 1) {
+    lockState.wheels[index] += 1
+  } else {
+    lockState.wheels[index] -= 1
+  }
 
   // When the lock is set to match the secretCombo
   // call the redirect() function with your name
